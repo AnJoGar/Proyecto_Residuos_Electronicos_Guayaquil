ae4edd2031a2c0aed03ce3891f34a9c4f80a703a
luisknight24 <luisknight24@gmail.com>
2024-10-02 14:44:10 -0500

    agregado

---
+++ Frontend_residuos_electronicos/modeloPredictivo/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { DatosEstadisticosActualesComponent } from './components/datos-estadisticos-actuales/datos-estadisticos-actuales.component';
+import { ModeloPrediccionComponent } from './components/modelo-prediccion/modelo-prediccion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AcercaDeComponent,
+    DatosEstadisticosActualesComponent,
+    ModeloPrediccionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
