b66dbe37a16902174c1d53fdae677c3f01bb2d77
AnJoGar <andjor2020@gmail.com>
2024-12-30 06:52:34 -0500

    proyectoReciclaje

--- Proyecto_residuos_electronicos/scripts/entrenamiento_modelo.py
+++ Proyecto_residuos_electronicos/scripts/entrenamiento_modelo.py
@@ -92,7 +92,7 @@ modelo_nn.save('modelo_residuos_electronicos.h5')
 np.save('historial_entrenamiento.npy', historial.history)
 
 # Guardar los resultados en SQL Server
-fecha_entrenamiento =  datetime(2024, 12, 22)
+fecha_entrenamiento =  datetime(2024, 12, 26)
 
 guardar_datos_entrenamiento(fecha_entrenamiento, mse, rmse, r2)
 print("Datos de entrenamiento guardados en SQL Server correctamente.")
